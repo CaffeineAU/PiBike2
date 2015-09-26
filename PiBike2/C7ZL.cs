c4dc398df26433d3d975fd1b81dac531497eba47
Derek Taprell <derektaprell@gmail.com>
2015-09-26 11:58:47 +1000

    First version that compiles

---
+++ PiBike2/C7ZL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace PiBike2
+{
+    class C7ZL
+    {
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+
+        public C7ZL()
+        {
+            InitGPIO();
+        }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                pin = null;
+                //GpioStatus.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+
+            pin = gpio.OpenPin(LED_PIN);
+
+            // Show an error if the pin wasn't initialized properly
+            if (pin == null)
+            {
+                //GpioStatus.Text = "There were problems initializing the GPIO pin.";
+                return;
+            }
+
+            pin.Write(GpioPinValue.High);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            //GpioStatus.Text = "GPIO pin initialized correctly.";  
+        }
+    }
+
+    
+}
